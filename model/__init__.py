6531f2a6039498651096c4b7ed5b757518c0ead6
G. Dylan Dickerson <gdicker1@uwyo.edu>
2019-12-17 14:49:40 -0700

    Made model folder act like Python package

---
+++ model/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+
+# __init__.py
+# Allow model folder to be interpreted as a Python package
+# by G. Dylan Dickerson
+# Created 17 Dec 2019
+
+from model.helpers import *
+from model.layers import *
+from model.model import *
+from model.networks import *
