8fa5f94d4bb50ff84aab97b431ecb0081be96212
George Dylan Dickerson <gdicker1@uwyo.edu>
2019-12-18 16:18:51 -0700

    Added kitti_loader_sequential to init

--- utils/__init__.py
+++ utils/__init__.py
@@ -9,6 +9,7 @@
 from utils.box_overlaps import *
 from utils.colorize import *
 from utils.kitti_loader import *
+from utils.kitti_loader_sequential import *
 from utils.utils import *
 from utils.preprocess import *
 from utils.data_aug import *
